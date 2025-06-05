48399ae16a7efc4f49312b586e6d104ea542e1aa
sukhairkar <sukantkhairkar@gmail.com>
2025-06-05 13:03:34 +0530

    Learning

---
+++ Training/odd.js
@@ -0,0 +1,6 @@
+let x=10
+if (x%2===0){
+    console.log("This number is Even");
+}else{
+    console.log("This number is Odd");
+}
---
+++ Training/test.js
@@ -0,0 +1,6 @@
+console.log("hello world");
+let x=5+9;;
+let y=2*5;
+let z=x==14 && y!=10;
+let g=x==9 || y!=10;
+console.log(z,g);
