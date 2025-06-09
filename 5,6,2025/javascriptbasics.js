0de091791a60db2894f24eb5c483b6b08c8e2bfc
sukhairkar <sukantkhairkar@gmail.com>
2025-06-10 00:52:23 +0530

    9 June,2025

---
+++ 5,6,2025/javascriptbasics.js
@@ -0,0 +1,41 @@
+//Function of Squaring of numbers
+
+function square(num){
+    let sq = num * num;
+    console.log(`Square root of ${num} is ${sq}`);
+}
+square(6);   
+
+// Callback Function
+
+function hello(name,func,a){
+    console.log(`Hello ${name}`);
+    func(a);
+}
+
+function bye(a){
+    console.log("See you next time!");
+    a();
+}
+
+function welcome(){
+    console.log("Welcome to the JavaScript world!");
+}
+
+hello("Sukant",bye,welcome)
+
+//  9|6|2025
+
+function delay(){
+    return new Promise(resolve => setTimeout(() => {
+        resolve("Delay completed!");
+    }, 500));
+}
+
+async function run(){
+    console.log("Waiting for delay...");
+    const result = await delay();
+    console.log(result);
+}
+
+run();
